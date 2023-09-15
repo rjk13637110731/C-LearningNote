c88f30593cf8ec7614b13adfe28c5d7b5aecbc34
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-09-15 19:36:58 +0800

    Create sizeof-int.cs

---
+++ Runoob/sizeof-int.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace TestApp
+{
+    class Test
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("sizeof(int) = {0}",sizeof(int));
+        }
+    }
+}

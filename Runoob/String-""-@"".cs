1fe5314db9fa7431afeb4b1bf4db146e213bb089
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-09-15 22:27:37 +0800

    Create String-""-@"".cs

---
+++ Runoob/String-""-@"".cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace test
+{
+    class Test
+    {
+        public static void Main(string[] args)
+        {
+            String s1 = "Runoob";
+            String s2 = @"\tabLeb";
+            Console.WriteLine("s1 = {0}",s1);
+            Console.WriteLine("s2 = {0}",s2);
+        }
+    }
+}

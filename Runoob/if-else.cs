0152279b0c3a2f500ccca08c42901bb0a19160db
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-09-23 16:23:02 +0800

    Create if-else.cs

---
+++ Runoob/if-else.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Test
+{
+    class Progran
+    {
+        public static void Main(string[] args)
+        {
+            int a = 100;
+            if (a < 20 )
+            {
+                Console.WriteLine("a小于20");
+            }
+            else
+            {
+                Console.WriteLine("a大于20");
+            }
+            Console.WriteLine("a的值为{0}",a);
+        }
+    }
+}

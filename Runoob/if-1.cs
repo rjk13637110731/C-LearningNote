e9a8d6601eb493ade31fdb0f0a983af076b4d8e5
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-09-23 16:12:19 +0800

    Create if-1.cs

---
+++ Runoob/if-1.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Test
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 10;
+            if (a < 20)
+            {
+                Console.WriteLine("a小于20");
+            }
+            Console.WriteLine("a的值为{0}",a);
+            Console.ReadLine();
+        }
+    }
+}

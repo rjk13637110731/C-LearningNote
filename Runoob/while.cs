ea93faff64b9359bc5caaf68bc988a8ec187ed86
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-09-24 20:14:05 +0800

    Create while.cs

---
+++ Runoob/while.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Loops
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 10;
+
+            while (a < 20)
+            {
+                Console.WriteLine("a的值是：{0}", a);
+                a++;
+            }
+        }
+    }
+}

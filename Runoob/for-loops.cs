c7861d6c496fcfaa6a6c66642a0c50f2d844dff4
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-09-24 20:21:34 +0800

    Create for-loops.cs

---
+++ Runoob/for-loops.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Loops
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            for (int a = 10; a < 20; a = a + 1)
+            {
+                Console.WriteLine("a的值：{0}",a);
+            }
+        }
+    }
+}

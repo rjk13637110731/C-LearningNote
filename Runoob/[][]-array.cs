ee31dafad0ee12982255675e95ac49f2333d4ab1
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-09-25 21:30:46 +0800

    Create [][]-array.cs

---
+++ Runoob/[][]-array.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Test
+{
+    class MyArry
+    {
+        public static void Main(string[] args)
+        {
+            int[][] a = new int[][]
+                { new int[] { 0, 0 }, new int[] { 1, 2 }, new int[] { 2, 4 }, new int[] { 3, 6 }, new int[] { 4, 8 } };
+            int i, j;
+            for (i = 0;i < 5;i++){
+                for (j = 0;j < 2;j++) {
+                    Console.WriteLine("a[{0}][{1}] = {2}",i,j,a[i][j]);
+                }
+            }
+        }
+    }
+}

d498fa7b652514c35afe07b7ef0a7e1f9c74908b
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-11-09 23:28:27 +0800

    Create Class1.cs

---
+++ LiuTieMengNote/Class1.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupperCalculator
+{
+    public class Class1
+    {
+        public static double Add(double a, double b) {
+            return a + b;
+        }
+        public static double Sub(double a, double b)
+        {
+            return a - b;
+        }
+        public static double Mul(double a, double b)
+        {
+            return a * b;
+        }
+        public static double Div(double a, double b)
+        {
+            if (b == 0)
+            {
+                return double.PositiveInfinity;
+            }
+            else {
+                return a / b;
+            }
+        }
+    }
+}

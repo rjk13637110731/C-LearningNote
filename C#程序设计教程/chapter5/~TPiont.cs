6cdbf1a90c5463e0f9bab1413f2234059cb3d1b3
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2024-05-14 16:50:39 +0800

    Create ~TPiont.cs

---
+++ C#程序设计教程/chapter5/~TPiont.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TPoint p1 = new TPoint();
+            Console.WriteLine("第一个点：");
+            p1.dispoint();
+            TPoint p2 = new TPoint(8,13);
+            Console.WriteLine("第二个点：");
+            p2.dispoint();
+        }
+    }
+
+    public class TPoint 
+    {
+        int x, y;
+        public TPoint() { }
+        public TPoint(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public void dispoint()
+        {
+            Console.WriteLine("{0},{1}",x,y);
+        }
+        ~TPoint()
+        {
+            Console.WriteLine("{0},{1}", x, y);
+        }
+    }
+}
